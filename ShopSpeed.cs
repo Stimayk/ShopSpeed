1ef7c411578bc33aac92ab9517fd0e52c39113fb
Stimayk <51941742+Stimayk@users.noreply.github.com>
2024-09-25 10:22:11 +0400

    Update ShopSpeed.cs

--- ShopSpeed.cs
+++ ShopSpeed.cs
@@ -11,7 +11,7 @@ namespace ShopSpeed
         public override string ModuleName => "[SHOP] Speed";
         public override string ModuleDescription => "";
         public override string ModuleAuthor => "E!N";
-        public override string ModuleVersion => "v1.0.0";
+        public override string ModuleVersion => "v1.0.1";
 
         private IShopApi? SHOP_API;
         private const string CategoryName = "Speed";
@@ -67,7 +67,7 @@ namespace ShopSpeed
             RegisterEventHandler<EventPlayerHurt>(PrePlayerHurtHandler);
         }
 
-        public void OnClientBuyItem(CCSPlayerController player, int itemId, string categoryName, string uniqueName,
+        public HookResult OnClientBuyItem(CCSPlayerController player, int itemId, string categoryName, string uniqueName,
             int buyPrice, int sellPrice, int duration, int count)
         {
             if (TryGetSpeedModifierValue(uniqueName, out float speedModifierValue))
@@ -79,9 +79,10 @@ namespace ShopSpeed
             {
                 Logger.LogError($"{uniqueName} has invalid or missing 'speedmodifier' in config!");
             }
+            return HookResult.Continue;
         }
 
-        public void OnClientToggleItem(CCSPlayerController player, int itemId, string uniqueName, int state)
+        public HookResult OnClientToggleItem(CCSPlayerController player, int itemId, string uniqueName, int state)
         {
             if (state == 1 && TryGetSpeedModifierValue(uniqueName, out float speedModifierValue))
             {
@@ -92,12 +93,14 @@ namespace ShopSpeed
             {
                 OnClientSellItem(player, itemId, uniqueName, 0);
             }
+            return HookResult.Continue;
         }
 
-        public void OnClientSellItem(CCSPlayerController player, int itemId, string uniqueName, int sellPrice)
+        public HookResult OnClientSellItem(CCSPlayerController player, int itemId, string uniqueName, int sellPrice)
         {
             playerSpeeds[player.Slot] = null!;
             ApplySpeedModifier(player, 1.0f); // Reset speed
+            return HookResult.Continue;
         }
 
 
